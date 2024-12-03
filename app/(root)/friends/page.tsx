4994641a09ebfba6c091febd7ab1af0020602e12
Arvin Buid <arvinbuid01@gmail.com>
2024-12-03 20:07:56 +0800

    Implement rendering the lists of request in the UI.

--- app/(root)/friends/page.tsx
+++ app/(root)/friends/page.tsx
@@ -1,15 +1,39 @@
+'use client';
+
 import ConversationFallback from '@/components/ui/shared/conversation/ConversationFallback';
 import ItemList from '@/components/ui/shared/item-list/ItemList';
 import React from 'react';
 import AddFriendDialog from './_components/AddFriendDialog';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { Loader2 } from 'lucide-react';
+import Request from './_components/Request';
 
-type Props = {};
+const Friends = () => {
+  const requests = useQuery(api.requests.get);
 
-const Friends = (props: Props) => {
   return (
     <>
       <ItemList title="Friends" action={<AddFriendDialog />}>
-        Friends Page
+        {requests ? (
+          requests.length === 0 ? (
+            <p className="h-full w-full flex items-center justify-center">
+              No friend requests
+            </p>
+          ) : (
+            requests.map((request) => (
+              <Request
+                key={request.request._id}
+                id={request.request._id}
+                imageUrl={request.sender.imageUrl}
+                username={request.sender.username}
+                email={request.sender.email}
+              />
+            ))
+          )
+        ) : (
+          <Loader2 />
+        )}
       </ItemList>
       <ConversationFallback />
     </>
---
+++ convex/requests.ts
@@ -0,0 +1,41 @@
+import { getUserByClerkId } from './_utils';
+import { query } from './_generated/server';
+
+export const get = query({
+  args: {},
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Unauthorized');
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new Error('User not found');
+    }
+
+    const requests = await ctx.db
+      .query('requests')
+      .withIndex('by_receiver', (q) => q.eq('receiver', currentUser._id))
+      .collect();
+
+    const requestsWithSender = await Promise.all(
+      requests.map(async (request) => {
+        const sender = await ctx.db.get(request.sender);
+
+        if (!sender) {
+          throw new Error('Sender could not be found');
+        }
+
+        return { sender, request };
+      }),
+    );
+
+    return requestsWithSender;
+  },
+});
